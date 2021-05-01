754ea720b6acc767e68399cd05da2d21f5979d26
Jonathan Chancey <jonathan22711@gmail.com>
2021-05-01 15:41:38 -0700

    add original favicon and set title

--- src/App.js
+++ src/App.js
@@ -1,14 +1,11 @@
 import './App.css';
 import Transactions from './components/Transactions'
-// import { ethers } from "ethers";
-// import detectEthereumProvider from '@metamask/detect-provider';
 
 function App() {
 
   return (
     <div className="App">
       <Transactions />
-      
     </div>
   );
 }
